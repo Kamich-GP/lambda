ead17a6d5e30a1298bb59db066eb2983ba459fc7
Your Name <you@example.com>
2024-01-25 21:47:38 +0500

    models views

--- index/urls.py
+++ index/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about-us', views.about),
     path('contact-us', views.contact),
     path('search', views.search_product),
+    path('category/<int:pk>', views.get_full_category),
+    path('product/<int:pk>', views.get_full_product),
+    path('product-not-found', views.pr_not_found),
 ]
